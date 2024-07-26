cc5514105d8a5e9559b5f67874d0b902bf3b354c
jaylen curry <curryjaylen24@gmail.com>
2024-07-26 14:01:56 -0400

    still works

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
         <h1>Welcome to the</h1>
         <p>choo choo chat</p>
       </div>
-      <img className={styles.imgTop} src="./public/HomePagePic.jpg"/>
-      <img className={styles.imgBottom} src="./public/HomePagePhoto.jpg"/>
+      <img className={styles.imgTop} src="./HomePagePic.jpg"/>
+      <img className={styles.imgBottom} src="./HomePagePhoto.jpg"/>
     </div>
   );
 };
